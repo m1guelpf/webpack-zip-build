7a3dc9c14b5dd1e051c26836754e2df0de10eedd
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-06-19 18:41:23 +0200

    ES6

--- index.js
+++ index.js
@@ -1,72 +1,75 @@
 'use strict';
 
-const path = require('path');
 const fs = require('fs');
 const archiver = require('archiver');
 
-function WebpackZipBuild(options) {
-  options = options || {};
-  if (typeof options === 'string') {
-    this.options = { output: options };
-  } else {
-    this.options = options;
+class WebpackZipBuild {
+  constructor(options) {
+    options = options || {};
+    if (typeof options === 'string') {
+      this.options = {
+        output: options
+      };
+    } else {
+      this.options = options;
+    }
+  }
+  apply(compiler) {
+    const options = this.options;
+    compiler.plugin('after-emit', (compiler, callback) => {
+      // Set output location
+      const output = options.output ?
+        options.output : compiler.options.output.path;
+      // Create archive stream
+      let archive;
+      let zip = true;
+      let tar = true;
+      if (options.format) {
+        if (typeof options.format === 'string') {
+          zip = (options.format === 'zip');
+          tar = (options.format === 'tar');
+        } else if (Array.isArray(options.format)) {
+          // Support later
+          zip = (options.format.indexOf('zip') != -1);
+          tar = (options.format.indexOf('tar') != -1);
+        }
+      }
+      if (zip) {
+        const ext = options.ext || 'zip';
+        archive = archiver('zip', {
+          zlib: {
+            level: 9
+          } // Sets the compression level.
+        });
+        archive.pipe(fs.createWriteStream(`${output}.${ext}`));
+      } else if (tar) {
+        const ext = options.ext || 'tar.gz';
+        archive = archiver('tar', {
+          gzip: true,
+          gzipOptions: {
+            level: 1
+          }
+        });
+        archive.pipe(fs.createWriteStream(`${output}.${ext}`));
+      }
+      if (options.entries) {
+        options.entries.forEach(() => {
+          archive.glob(entry);
+        });
+      } else {
+        for (let asset in compiler.assets) {
+          if (compiler.assets.hasOwnProperty(asset)) {
+            archive.append(fs.createReadStream(compiler.assets[asset].existsAt), {
+              name: asset
+            });
+          }
+        }
+      }
+      archive.finalize();
+      callback();
+    });
   }
 }
 
-WebpackZipBuild.prototype.apply = function (compiler) {
-  const options = this.options;
-
-  compiler.plugin('after-emit', function (compiler, callback) {
-    // Set output location
-    const output = options.output ?
-      options.output : compiler.options.output.path;
-
-    // Create archive stream
-    let archive;
-    let zip = true;
-    let tar = true;
-    if (options.format) {
-      if (typeof options.format === 'string') {
-        zip = (options.format === 'zip');
-        tar = (options.format === 'tar');
-      } else if (Array.isArray(options.format)) {
-        // Support later
-        zip = (options.format.indexOf('zip') != -1);
-        tar = (options.format.indexOf('tar') != -1);
-      }
-    }
-    if (zip) {
-      const ext = options.ext || 'zip'
-      archive = archiver('zip', {
-        zlib: { level: 9 } // Sets the compression level.
-      });
-      archive.pipe(fs.createWriteStream(`${output}.${ext}`));
-    } else if (tar) {
-      const ext = options.ext || 'tar.gz'
-      archive = archiver('tar', {
-        gzip: true,
-        gzipOptions: {
-          level: 1
-        }
-      });
-      archive.pipe(fs.createWriteStream(`${output}.${ext}`));
-    }
-
-    if (options.entries) {
-      foreach (entry in options.entries) {
-        archive.glob(entry);
-      }
-    } else {
-      for (let asset in compiler.assets) {
-        if (compiler.assets.hasOwnProperty(asset)) {
-          archive.append(fs.createReadStream(compiler.assets[asset].existsAt), { name: asset });
-        }
-      }
-    }
-    archive.finalize();
-
-    callback();
-  });
-}
 
 module.exports = WebpackZipBuild;
